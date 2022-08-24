63ecbe2523c0ef70b2938e59fb4ed30d657aaf13
blockodyssey-sckim <72206135+kimseongchan-kr@users.noreply.github.com>
2022-08-24 20:39:07 +0900

    mod: 컴포넌트 분리 작업 및 적용

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,4 +1,6 @@
 import DrawForm from './drawForm/index.js';
 import Toggle from './toggle/index.js';
+import WinningInput from './winningInput/index.js';
+import InquiryForm from './InquiryForm/index.js';
 
-export { DrawForm, Toggle };
+export { DrawForm, Toggle, WinningInput, InquiryForm };
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import { DrawForm, Toggle } from './components/index.js';
+import { DrawForm, Toggle, WinningInput, InquiryForm } from './components/index.js';
 
 import getLottoList from './lotto/index.js';
 import { getLottoAvailableQuantity } from './lotto/validation/index.js';
@@ -14,12 +14,12 @@ const lottoSheetForm = document.querySelector('.lotto-sheet-form');
 const lottoPriceInput = document.querySelector('.lotto-price-input');
 
 const winningNumberForm = document.querySelector('#winning-number-form');
-
 const winningNumberInput = document.querySelectorAll('.winning-number');
-
 const bonusNumberInput = document.querySelector('.bonus-number');
 
 const target = { title: listTitleElement, show: showListElement, hide: hideListElement };
+
+const winningNumberArray = [...winningNumberInput, bonusNumberInput];
 
 DrawForm({
   target,
@@ -29,41 +29,8 @@ DrawForm({
   logic: getLottoList,
 });
 
+InquiryForm({ form: winningNumberForm, numberInput: winningNumberArray });
+
 Toggle({ target, checkbox: lottoNumberToggleButton });
 
-const winningNumberArray = [...winningNumberInput, bonusNumberInput];
-
-winningNumberForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const inputTextArray = winningNumberArray
-    .map((element) => parseInt(element.value, 10))
-    .sort()
-    .reduce((previousValue, currentValue) => {
-      if (!previousValue) return false;
-      return previousValue === currentValue;
-    });
-
-  if (!inputTextArray) {
-    alert('로또 번호에는 중복된 숫자를 입력할 수 없습니다.');
-  }
-});
-
-winningNumberInput.forEach((element, index) => {
-  element.addEventListener('keyup', (event) => {
-    const { keyCode, target } = event;
-
-    if ((keyCode > 47 || keyCode < 58) && target.value.length >= 2) {
-      target.value = target.value.slice(0, 2);
-      winningNumberArray[index + 1].value = null;
-      winningNumberArray[index + 1].focus();
-    }
-  });
-});
-
-bonusNumberInput.addEventListener('keyup', (event) => {
-  const { keyCode, target } = event;
-
-  if ((keyCode > 47 || keyCode < 58) && target.value.length >= 2) {
-    target.value = target.value.slice(0, 2);
-  }
-});
+WinningInput({ numberInput: winningNumberArray });
